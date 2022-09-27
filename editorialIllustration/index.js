b463bedffc97358ea47719fe0adb19eebc0c76d2
aaryakini <kinia313@newschool.edu>
2022-09-26 21:12:18 -0400

    test test

--- editorialIllustration/index.js
+++ editorialIllustration/index.js
@@ -1,6 +1,7 @@
+let event = document.getElementById("outer").event;
 let z = 0;
 let max = 150;
-let min = 20;
+let min = 50;
 let days = document.getElementsByClassName("day");
 console.log(days);
 let free = Math.floor(Math.random() * days.length);
@@ -8,16 +9,21 @@ console.log(days[free].id);
 
 function makeEvent(name){
   let activeDay = document.getElementById(name);
-  if (days[free] !== activeDay && z < 101){
+  if (days[free] !== activeDay && z < 101 && Math.random()<0.5){
     let newDiv = document.createElement("DIV");
     newDiv.classList.add("event");
     newDiv.style.width = "73px";
-    newDiv.style.height = makeRandomSize(min, max) + "px";
+
+    let h = makeRandomSize(min, max);
+    newDiv.style.height = h + "px";
+
     newDiv.style.position = "absolute";
-    newDiv.style.top = event.clientY + "px"; //this is causing PROBLEMS!!!!!!!!
+
+    let pos = event.clientY - 241; //what do I subtract???
+    newDiv.style.top = pos + "px"; //this is causing PROBLEMS!!!!!!!!
     newDiv.style.filter = `drop-shadow(0px 0px 5px #D333FF)`;
     newDiv.style.borderRadius = "6px"
-    newDiv.style.transform = `translateY(-50%)`;
+    // newDiv.style.transform = `translateY(-50%)`;
     newDiv.style.background = `hsla(${Math.random()*360}, 100%, 60%, 60%)`;
     activeDay.appendChild(newDiv); //how to append at a specific position?
     z = z + 1;
